5ec37443a9b941ce548e2b833a7f67eb3bb84f91
fandi9155 <61773118+fandi9155@users.noreply.github.com>
2020-03-21 20:29:38 +0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TugasClassdanObjeck
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          taxi taksi = new taxi();
+
+            taksi.DriverName();
+            taksi.OnDuty();
+            taksi.NumberOfPpassenger();
+
+
+            taksi.PickUpPassenger();
+            taksi.DropOffPasengger();
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ taxi.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TugasClassdanObjeck
+{
+    class taxi
+    {
+        string nama;
+        int nomor;
+        int duty;
+
+        public void DriverName()
+        {
+            Console.WriteLine("Driver Name : jono");
+        }
+
+        public void OnDuty()
+        {
+            Console.WriteLine("On Duty : Yes");
+        }
+
+        public void NumberOfPpassenger()
+        {
+            Console.WriteLine("Number Of Passenger : 10");
+        }
+
+        public void PickUpPassenger()
+        {
+            Console.WriteLine("jono Sedang Menjemput Penumpang");
+        }
+
+        public void DropOffPasengger()
+        {
+            Console.WriteLine("jono Sedang Mengantar Penumpang");
+        }
+
+
+    }
+}
